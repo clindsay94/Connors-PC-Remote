21a37bc809089b2b24e993b59809ea93b5849d51
Connor <115328188+clindsay94@users.noreply.github.com>
2025-11-18 14:16:11 -0700

    Create QuickActionsPage.xaml.cs

---
+++ CPCRemote.UI/QuickActionsPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace CPCRemote.UI
+{
+    public sealed partial class QuickActionsPage : Page
+    {
+        public QuickActionsPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
