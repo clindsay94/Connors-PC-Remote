ff2cc7cf4f13df527d6692c0d86b3ad23c6ceaa2
Connor <115328188+clindsay94@users.noreply.github.com>
2025-11-18 14:18:26 -0700

    Create ServiceManagementPage.xaml.cs

---
+++ CPCRemote.UI/ServiceManagementPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace CPCRemote.UI
+{
+    public sealed partial class ServiceManagementPage : Page
+    {
+        public ServiceManagementPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
