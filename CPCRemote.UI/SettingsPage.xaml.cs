248bb586fa876c9762976fc24dea6ddd2f3937e4
Connor <115328188+clindsay94@users.noreply.github.com>
2025-11-18 14:19:18 -0700

    Create SettingsPage.xaml.cs

---
+++ CPCRemote.UI/SettingsPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace CPCRemote.UI
+{
+    public sealed partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
